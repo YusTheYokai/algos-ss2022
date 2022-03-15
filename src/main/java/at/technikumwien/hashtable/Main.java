05c1c9e601329249dec84eca8733fa74e05c3e58
Nico Lerchl <nico@lerchl.one>
2022-03-15 18:48:59 +0100

    feat 2: Stock und History Class

---
+++ src/main/java/at/technikumwien/hashtable/Main.java
@@ -0,0 +1,8 @@
+package at.technikumwien.hashtable;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Stock s = new Stock("MSFT", "Microsoft Corporation", "MSFT");
+    }
+}
