30f05aa61c56f84a7ca6d15fe35fb1a06ef3e599
TORz69 <91666614+TORz69@users.noreply.github.com>
2022-03-15 19:18:13 +0100

    Feat 1: Hashtable Zwischenstand

--- src/main/java/at/technikumwien/hashtable/Main.java
+++ src/main/java/at/technikumwien/hashtable/Main.java
@@ -4,5 +4,8 @@ public class Main {
     
     public static void main(String[] args) {
         Stock s = new Stock("MSFT", "Microsoft Corporation", "MSFT");
+        Hashtable h = new Hashtable(2003);
+        h.add(s);
+        System.out.println(h.get(s.getName()).getAbbreviation());
     }
 }
