058494b6ab75b0d724f60c33c1fc68212e930798
TORz69 <91666614+TORz69@users.noreply.github.com>
2022-03-19 17:36:10 +0100

    Feat 10: SEARCH-Befehl

---
+++ src/main/java/at/technikumwien/hashtable/command/SearchCommand.java
@@ -0,0 +1,45 @@
+package at.technikumwien.hashtable.command;
+
+import java.util.Scanner;
+
+import at.technikumwien.hashtable.Hashtable;
+import at.technikumwien.hashtable.Stock;
+
+public class SearchCommand implements Runnable {
+
+    private final Hashtable<String, String> abbrHashtable;
+    private final Hashtable<String, Stock> stockHashtable;
+    private final Scanner scanner;
+
+    // //////////////////////////////////////////////////////////////////////////
+    // Init
+    // //////////////////////////////////////////////////////////////////////////
+
+    public SearchCommand(Hashtable<String, String> abbrHashtable, Hashtable<String, Stock> stockHashtable, Scanner scanner) {
+        this.abbrHashtable = abbrHashtable;
+        this.stockHashtable = stockHashtable;
+        this.scanner = scanner;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Name/Abbreviation:");
+        String key = scanner.nextLine();
+        
+        Stock stock;
+        String abbr = abbrHashtable.get(key);
+        if (abbr == null) {
+            stock = stockHashtable.get(key);
+        } else {
+            stock = stockHashtable.get(abbr);
+        }
+        
+        if (stock == null) {
+            System.err.println("No such stock");
+            return;
+        }
+
+        System.out.println(stock.getHistories().get(0));
+    }
+    
+}
