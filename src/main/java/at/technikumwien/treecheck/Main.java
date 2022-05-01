4950c01f71f605bdd3e848ec8f728af77bbf6a7c
Nico Lerchl <nico@lerchl.one>
2022-05-01 14:05:43 +0200

    feat 33: Subtree finden

--- src/main/java/at/technikumwien/treecheck/Main.java
+++ src/main/java/at/technikumwien/treecheck/Main.java
@@ -9,49 +9,60 @@ import java.util.stream.Collectors;
 public class Main {
 
     public static void main(String[] args) {
-        args = new String[]{"src/main/resources/treecheck/parentTree", "src/main/resources/treecheck/subtree"};
-
         if (args.length == 0) {
             error("Please provide a path to a file containing a tree!", 1);
             System.exit(1);
         } else if (args.length > 2) {
             error("A maximum of two paths to a file containing a tree is allowed!", 2);
         }
-        
+
+        action(args);
+    }
+
+    private static void action(String[] args) {
         try {
             List<String> lines = Files.readAllLines(Paths.get(args[0]));
             List<Integer> integers = lines.stream().map(Integer::parseInt).toList();
             Tree tree = new Tree(integers);
-            
             if (args.length == 1) {
-                tree.print();
-                System.out.println("AVL: " + (tree.isAvl() ? "yes" : "no"));
-                System.out.println("min: " + tree.min() + ", max: " + tree.max() + ", avg: " + tree.avg());
+                singleTree(tree);
             } else {
-                lines = Files.readAllLines(Paths.get(args[1]));
-                integers = lines.stream().map(Integer::parseInt).toList();
-                Tree subtree = new Tree(integers);
-
-                if (subtree.count() == 1) {
-                    var value = subtree.getRoot().getValue();
-                    var result = tree.search(value);
-                    System.out.println(value + " found " + result.getSecond().stream().map(i -> i.toString()).collect(Collectors.joining(", "))); // NOSONAR
-                } else {
-                    // var result = subtree.subtreeOf(tree);
-                    // System.out.println(result.getFirst());
-
-                    System.out.println(tree.contains(subtree));
-                }
+                subtree(tree, args);
             }
         } catch (IOException e) {
             error("File could not be read!", 3);
-       } catch (NumberFormatException e) {
+        } catch (NumberFormatException e) {
             error("Tree contains char sequence that could not be parsed to an integer!", 4);
-       }
+        }
+    }
+
+    private static void singleTree(Tree tree) {
+        tree.print();
+        System.out.println("AVL: " + (tree.isAvl() ? "yes" : "no")); // NOSONAR
+        System.out.println("min: " + tree.min() + ", max: " + tree.max() + ", avg: " + tree.avg()); // NOSONAR
+    }
+
+    private static void subtree(Tree tree, String[] args) throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get(args[1]));
+        List<Integer> integers = lines.stream().map(Integer::parseInt).toList();
+        Tree subtree = new Tree(integers);
+
+        if (subtree.count() == 1) {
+            var value = subtree.getRoot().getValue();
+            var result = tree.search(value);
+
+            if (Boolean.TRUE.equals(result.getFirst())) {
+                System.out.println(value + " found " + result.getSecond().stream().map(i -> i.toString()).collect(Collectors.joining(", "))); // NOSONAR
+            } else {
+                System.out.println(value + " not found!"); // NOSONAR
+            }
+        } else {
+            System.out.println(tree.contains(subtree) ? "Subtree found!" : "Subtree not found!"); // NOSONAR
+        }
     }
 
     private static void error(String message, int code) {
-        System.err.println(message);
+        System.err.println(message); // NOSONAR
         System.exit(code);
     }
 }
--- src/main/java/at/technikumwien/treecheck/Tree.java
+++ src/main/java/at/technikumwien/treecheck/Tree.java
@@ -1,13 +1,14 @@
 package at.technikumwien.treecheck;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
+/**
+ * Stellt ein {@link Node}gespann dar.
+ */
 public class Tree {
 
     private static final Pair<Boolean, List<Integer>> SEARCH_NOT_FOUND = new Pair<>(false, new ArrayList<>());
-    private static final Pair<Boolean, Node> SUBTREE_NOT_FOUND = new Pair<>(false, null);
 
     private Node root;
 
@@ -23,8 +24,42 @@ public class Tree {
     // Methoden
     // //////////////////////////////////////////////////////////////////////////
 
-    // Hinzufügen
+    // /////////////////////////////////////
+    // Ausgeben
+    // /////////////////////////////////////
 
+    /**
+     * Gibt den Tree aus.
+     */
+    public void print() {
+        printTraverse(root);
+    }
+
+    /**
+     * Gibt ein {@link Node}gespann aus.
+     * @param node das {@link Node}gespann
+     */
+    private void printTraverse(Node node) {
+        if (node.getRight() != null) {
+            printTraverse(node.getRight());
+        }
+        if (node.getLeft() != null) {
+            printTraverse(node.getLeft());
+        }
+
+        final int bal = node.balance();
+        final boolean violation = bal > 1;
+        System.out.println("bal(" + node.getValue() + ") = " + bal + (violation ? " (AVL violation!)" : "")); // NOSONAR
+    }
+
+    // /////////////////////////////////////
+    // Hinzufügen
+    // /////////////////////////////////////
+
+    /**
+     * Fügt einen Wert dem Tree hinzu.
+     * @param value der Wert
+     */
     public void add(int value) {
         if (root == null) {
             root = new Node(value);
@@ -37,7 +72,12 @@ public class Tree {
         }
     }
 
-    private void addTraverse(Node node, int value) {
+    /**
+     * Fügt einem {@link Node}gespann einen Wert hinzu. 
+     * @param node das {@link Node}gespann
+     * @param value der Wert
+     */
+    private static void addTraverse(Node node, int value) {
         if (node.getValue() == value) {
             throw new IllegalArgumentException();
         } else if (node.getValue() < value) {
@@ -57,11 +97,20 @@ public class Tree {
 
     // Prüfung AVL
 
+    /**
+     * Prüft, ob der Tree ein AVL-Tree ist.
+     * @return {@code true}, wenn ja, {@code false}, wenn nicht.
+     */
     public boolean isAvl() {
         return isAvlTraverse(root);
     }
 
-    private boolean isAvlTraverse(Node node) {
+    /**
+     * Prüft, ob ein {@link Node}gespann ein AVL-Tree ist.
+     * @param node das {@link Node}gespann
+     * @return {@code true}, wenn ja, {@code false}, wenn nicht.
+     */
+    private static boolean isAvlTraverse(Node node) {
         boolean violationRight = false;
         boolean violationLeft = false;
 
@@ -72,16 +121,27 @@ public class Tree {
             violationLeft = isAvlTraverse(node.getLeft());
         }
 
-        return node.bal() < 2 && !violationRight && !violationLeft;
+        return node.balance() < 2 && !violationRight && !violationLeft;
     }
 
+    // /////////////////////////////////////
     // Minimum
+    // /////////////////////////////////////
 
+    /**
+     * Findet das Minmum des Trees.
+     * @return das Minimum
+     */
     public int min() {
         return minTraverse(root);
     }
 
-    private int minTraverse(Node node) {
+    /**
+     * Findet das Minmum in einem {@link Node}gespann.
+     * @param node das {@link Node}gespann
+     * @return das Minimum
+     */
+    private static int minTraverse(Node node) {
         int minRight = Integer.MAX_VALUE;
         int minLeft = Integer.MAX_VALUE;
 
@@ -97,11 +157,20 @@ public class Tree {
 
     // Maximum
 
+    /**
+     * Findet das Maximum des Trees.
+     * @return das Maximum
+     */
     public int max() {
         return maxTraverse(root);
     }
 
-    private int maxTraverse(Node node) {
+    /**
+     * Findet das Maximum in einem {@link Node}gespann.
+     * @param node das {@link Node}gespann
+     * @return das Maximum
+     */
+    private static int maxTraverse(Node node) {
         int maxRight = Integer.MIN_VALUE;
         int maxLeft = Integer.MIN_VALUE;
 
@@ -115,13 +184,24 @@ public class Tree {
         return Math.max(Math.max(maxRight, maxLeft), node.getValue());
     }
 
+    // /////////////////////////////////////
     // Summe
+    // /////////////////////////////////////
 
+    /**
+     * Berechnet die Summe des Trees.
+     * @return die Summe
+     */
     private float sum() {
         return sumTraverse(root);
     }
 
-    private float sumTraverse(Node node) {
+    /**
+     * Berechnet die Summe eines {@link Node}gespanns.
+     * @param node das {@link Node}gespann
+     * @return die Summe
+     */
+    private static float sumTraverse(Node node) {
         float left = 0f;
         float right = 0f;
 
@@ -135,13 +215,24 @@ public class Tree {
         return node.getValue() + left + right;
     }
 
+    // /////////////////////////////////////
     // Anzahl
+    // /////////////////////////////////////
 
+    /**
+     * Zählt die Anzahl der Werte im Tree.
+     * @return die Anzahl
+     */
     public int count() {
         return countTraverse(root);
     }
 
-    private int countTraverse(Node node) {
+    /**
+     * Zählt die Anzahl der Werte in einem {@link Node}gespann.
+     * @param node das {@link Node}gespann
+     * @return die Anzahl
+     */
+    private static int countTraverse(Node node) {
         int left = 0;
         int right = 0;
 
@@ -155,53 +246,57 @@ public class Tree {
         return 1 + left + right;
     }
 
+    // /////////////////////////////////////
     // Durchschnitt
+    // /////////////////////////////////////
 
+    /**
+     * Berechnet den Durchschnitt des Trees.
+     * @return den Durchschnitt
+     * @see {@link #sum()} {@link #count()}
+     */
     public float avg() {
         return sum() / count();
     }
 
-    // Ausgeben
+    // /////////////////////////////////////
+    // Suchen
+    // /////////////////////////////////////
 
-    public void print() {
-        printTraverse(root);
-    }
-
-    private void printTraverse(Node node) {
-        if (node.getRight() != null) {
-            printTraverse(node.getRight());
-        }
-        if (node.getLeft() != null) {
-            printTraverse(node.getLeft());
-        }
-
-        final int bal = node.bal();
-        final boolean violation = bal > 1;
-        System.out.println("bal(" + node.getValue() + ") = " + bal + (violation ? " (AVL violation!)" : ""));
-    }
-    
-    // Search
-
+    /**
+     * Sucht einen Wert im Tree.
+     * @param value der Wert
+     * @return ein {@link Pair} mit einem Boolean, ob der Wert gefunden wurde
+     * und einer Liste aller Werte die auf dem Weg zum gesuchten Werte traversiert wurden.
+     */
     public Pair<Boolean, List<Integer>> search(int value) {
         return search(value, root, SEARCH_NOT_FOUND);
     }
 
-    private Pair<Boolean, List<Integer>> search(int value, Node node, Pair<Boolean, List<Integer>> p) {
-        p.getSecond().add(node.getValue());
+    /**
+     * Prüft, ob sich ein Wert in einem {@link Node}gespann befindet.
+     * @param value der Wert
+     * @param node das {@link Node}gespann
+     * @param pair das Ergebnis, welches auch zurückgegeben wird. Hier wird gespeichert,
+     * ob die Suche erfolgreich war und alle Werte die bis zum gefundnen Wert traversiert wurden.
+     * @return das pair
+     */
+    private Pair<Boolean, List<Integer>> search(int value, Node node, Pair<Boolean, List<Integer>> pair) {
+        pair.getSecond().add(node.getValue());
 
         if (node.getValue() == value) {
-            p.setFirst(true);
-            return p;
+            pair.setFirst(true);
+            return pair;
         }
 
         var resultLeft = SEARCH_NOT_FOUND;
         var resultRight = SEARCH_NOT_FOUND;
 
         if (node.getLeft() != null) {
-            resultLeft = search(value, node.getLeft(), copyPair(p));
+            resultLeft = search(value, node.getLeft(), copyPair(pair));
         }
         if (node.getRight() != null) {
-            resultRight = search(value, node.getRight(), copyPair(p));
+            resultRight = search(value, node.getRight(), copyPair(pair));
         }
 
         if (Boolean.TRUE.equals(resultLeft.getFirst())) {
@@ -213,38 +308,69 @@ public class Tree {
         }
     }
 
+    /**
+     * Erstellt eine Kopie eines {@link Pair}s.
+     * @param pair das {@link Pair}
+     * @return die Kopie
+     */
     private Pair<Boolean, List<Integer>> copyPair(Pair<Boolean, List<Integer>> pair) {
         List<Integer> copiedList = new ArrayList<>();
         copiedList.addAll(pair.getSecond());
         return new Pair<>(pair.getFirst().booleanValue(), copiedList);
     }
 
+    // /////////////////////////////////////
+    // Subtree
+    // /////////////////////////////////////
+
+    /**
+     * Prüft, ob ein {@link Tree} ein Subtree ist.
+     * @param subtree der {@link Tree}
+     * @return {@code true}, wenn es ein Subtree ist, {@code false}, wenn nicht.
+     */
     public boolean contains(Tree subtree) {
         return contains(subtree.root, root);
     }
 
+    /**
+     * Prüft, ob sich das {@link Node}gespann einer Subtree-Node
+     * im {@link Node}gespann einer Parent-Tree-Node befindet.
+     * @param subtreeNode die Subtree-Node
+     * @param parentTreeNode die Parent-Tree-Node
+     * @return {@code true}, wenn sich das Subtree-Nodegespann im Parent-Tree-Nodegespann befindet, {@code false}, wenn nicht.
+     */
     private boolean contains(Node subtreeNode, Node parentTreeNode) {
         Node found = containsTraverse(subtreeNode.getValue(), parentTreeNode);
 
         if (found == null) {
             return false;
-        } else if (subtreeNode.getLeft() == null && subtreeNode.getRight() == null) {
-            return true;
         }
 
         boolean left = false;
         boolean right = false;
 
-        if (subtreeNode.getLeft() != null && found.getLeft() != null) {
+        if (subtreeNode.getLeft() == null) {
+            left = true;
+        } else if (found.getLeft() != null) {
             left = contains(subtreeNode.getLeft(), found.getLeft());
         }
-        if (subtreeNode.getRight() != null && found.getRight() != null) {
+
+        if (subtreeNode.getRight() == null) {
+            right = true;
+        } else if (found.getRight() != null) {
             right = contains(subtreeNode.getRight(), found.getRight());
         }
 
-        return left || right;
+        return left && right;
     }
 
+    /**
+     * Prüft, ob sich ein Wert in einem {@link Node}gespann befindet.
+     * @param value der Wert
+     * @param node das {@link Node}gespann
+     * @return die {@link Node}, dessen Wert gesucht wurde.
+     * {@code null}, wenn der Wert nicht gefunden wurde.
+     */
     private Node containsTraverse(int value, Node node) {
         if (node.getValue() == value) {
             return node;
@@ -267,71 +393,6 @@ public class Tree {
         } else {
             return null;
         }
-    }
-
-    // Subtree
-
-    public Pair<Boolean, List<Integer>> subtreeOf(Tree parentTree) {
-        var subtreeFlat = flat();
-        var parentTreeFlat = parentTree.flat();
-        parentTreeFlat.removeIf(i -> !subtreeFlat.contains(i));
-
-        if (parentTreeFlat.size() != subtreeFlat.size()) {
-            return new Pair<>(false, Collections.emptyList());
-        }
-
-        for (int i = 0; i < subtreeFlat.size(); i++) {
-            if (!subtreeFlat.get(i).equals(parentTreeFlat.get(i)) ) {
-                return new Pair<>(false, Collections.emptyList());
-            }
-        }
-        return new Pair<>(true, Collections.emptyList());
-    }
-
-    private List<Integer> flat() {
-        List<Integer> list = new ArrayList<>();
-        flatTraverse(root, list);
-        return list;
-    }
-
-    private void flatTraverse(Node node, List<Integer> list) {
-        if (node.getLeft() != null) {
-            flatTraverse(node.getLeft(), list);
-        }
-        if (node.getRight() != null) {
-            flatTraverse(node.getRight(), list);
-        }
-        list.add(node.getValue());
-    }
-
-    private boolean isSubtreeTraverse(Node n1, Node n2) {
-        if (n2 == null) {
-            return true;
-        }
-        
-        if (n1 == null) {
-            return false;
-        }
-        
-        if (equalsTraverse(n1, n2)) {
-            return true;
-        }
-
-        return isSubtreeTraverse(n1.getLeft(), n2) || isSubtreeTraverse(n1.getRight(), n2);
-    }
-
-    private boolean equalsTraverse(Node n1, Node n2) {
-        if (n1 == null && n2 == null) {
-            return true;
-        } 
-        
-        if (n1 == null || n2 == null) {
-            return false;
-        }
-
-        return n1.getValue() == n2.getValue() &&
-                equalsTraverse(n1.getLeft(), n2.getLeft()) &&
-                equalsTraverse(n1.getRight(), n2.getRight());
     }
 
     // //////////////////////////////////////////////////////////////////////////
